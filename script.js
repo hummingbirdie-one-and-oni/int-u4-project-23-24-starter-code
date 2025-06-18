01f1799abd257d36b86c8d357dd4b3bfb61dc3b4
hummingbirdie-one-and-oni <tinyslowpants@gmail.com>
2025-06-18 11:08:41 -0500

    guh

--- script.js
+++ script.js
@@ -1,1 +1,105 @@
 // JavaScript goes here.
+let button_1 = document.querySelector(".opt-1");
+let button_2 = document.querySelector(".opt-2");
+let button_3 = document.querySelector(".opt-3");
+let button_4 = document.querySelector(".opt-4");
+let button_5 = document.querySelector(".opt-5");
+let button_6 = document.querySelector(".opt-6");
+let button_7 = document.querySelector(".opt-7");
+let button_8 = document.querySelector(".opt-8");
+let button_9 = document.querySelector(".opt-9");
+let button_10 = document.querySelector(".opt-10");
+let results = document.querySelector(".result");
+let results_2 = document.querySelector(".result-2");
+let form = document.querySelector(".form")
+let hidden = document.querySelector(".hidden")
+/*let results_3 = document.querySelector(".result-3");
+let results_4 = document.querySelector(".result-4");
+let results_5 = document.querySelector(".result-5");
+let results_6 = document.querySelector(".result-6");
+let results_7 = document.querySelector(".result-7");
+let results_8 = document.querySelector(".result-8");
+let results_9 = document.querySelector(".result-9");
+let results_10 = document.querySelector(".result-10");
+let questions = document.querySelector(".questions");
+*/
+//button_1.addEventListener('click', function() {
+
+
+//})
+
+//if (results < 10) {
+    
+//}
+
+//const button = document.getElementById('opt-1');
+   // button.value = '4';
+
+   button_1.addEventListener('click', function () {
+        results.classList.add("hidden");
+        results.classList.remove("hidden");
+        console.log("clicked");
+   })
+
+   button_4.addEventListener('click', function () {
+        results.classList.add("hidden");
+        results.classList.remove("hidden");
+        console.log("clicked");
+        
+   })
+
+   button_6.addEventListener('click', function () {
+        results.classList.add("hidden");
+        results.classList.remove("hidden");
+        console.log("clicked");
+   })
+
+   button_7.addEventListener('click', function () {
+        results.classList.add("hidden");
+        results.classList.remove("hidden");
+        console.log("clicked");
+   })
+
+   button_9.addEventListener('click', function () {
+        results.classList.add("hidden");
+        results.classList.remove("hidden");
+        console.log("clicked");
+        results_2.classList.add("hidden");
+   })
+
+   button_2.addEventListener('click', function () {
+        results_2.classList.add("hidden");
+        results_2.classList.remove("hidden");
+        console.log("clicked");
+        results.classList.add("hidden");
+   })
+
+   button_3.addEventListener('click', function () {
+        results_2.classList.add("hidden");
+        results_2.classList.remove("hidden");
+        console.log("clicked");
+        results.classList.add("hidden");
+   })
+
+   button_5.addEventListener('click', function () {
+        results_2.classList.add("hidden");
+        results_2.classList.remove("hidden");
+        console.log("clicked");
+        results.classList.add("hidden");
+   })
+
+   button_8.addEventListener('click', function () {
+        results_2.classList.add("hidden");
+        results_2.classList.remove("hidden");
+        console.log("clicked");
+        results.classList.add("hidden");
+   })
+
+   button_10.addEventListener('click', function () {
+        results_2.classList.add("hidden");
+        results_2.classList.remove("hidden");
+        console.log("clicked");
+        results.classList.add("hidden");
+   }) 
+
+   /* how can i make 'uchiha' dissapear when 'uzumaki' appears. and vice versa? */
